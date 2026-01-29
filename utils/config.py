f759896506151a23a4e3680680b4b9fccea32dce
Lyadh <Lyadh>
2026-01-29 10:47:35 +0530

    sseh

--- utils/config.py
+++ utils/config.py
@@ -66,26 +66,26 @@ CHANNEL_IDS: List[int] = []
 Update_Interval: int = 3600 #int(_cfg.get("Update_Interval", 3600))
 
 # Basic validation
-if not STEAM_API_KEY:
-    try:
-        os.getenv("steam_api_key")
-    except:
-        logger.error("Missing Steam API key. Set 'steam_api_key' in %s or STEAM_API_KEY env var", CONFIG_FILE)
-        raise SystemExit(1)
+#if not STEAM_API_KEY:
+try:
+    os.getenv("steam_api_key")
+except:
+    logger.error("Missing Steam API key. Set 'steam_api_key' in %s or STEAM_API_KEY env var", CONFIG_FILE)
+    raise SystemExit(1)
 
-if not BOT_TOKEN:
-    try:
-        os.getenv("bot_token")
-    except:
-        logger.error("Missing Discord bot token. Set 'bot_token' in %s or DISCORD_BOT_TOKEN env var", CONFIG_FILE)
-        raise SystemExit(1)
+#if not BOT_TOKEN:
+try:
+    os.getenv("bot_token")
+except:
+    logger.error("Missing Discord bot token. Set 'bot_token' in %s or DISCORD_BOT_TOKEN env var", CONFIG_FILE)
+    raise SystemExit(1)
 
-if not CHANNEL_IDS:
-    try:
-        CHANNEL_IDS = [int(x) for x in os.getenv("channel_ids", "").split(",") if x]
-    except:
-        logger.warning("No channel IDs configured (CONFIG_FILE=%s). CHANNEL_IDS is empty.", CONFIG_FILE)
-        pass
+#if not CHANNEL_IDS:
+try:
+    CHANNEL_IDS = [int(x) for x in os.getenv("channel_ids", "").split(",") if x]
+except:
+    logger.warning("No channel IDs configured (CONFIG_FILE=%s). CHANNEL_IDS is empty.", CONFIG_FILE)
+    pass
 
 
 #logger.info("Configuration loaded: Update_Interval=%d seconds, CHANNEL_IDS=%r", Update_Interval, CHANNEL_IDS)
